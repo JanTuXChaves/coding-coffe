2d2b11bfe08aa3beb028e1a9e45ebd6bf2c1a604
Jan Chaves <82485567+JanTuXChaves@users.noreply.github.com>
2025-05-06 16:46:20 -0300

    Update import os.py

--- import os.py
+++ import os.py
@@ -18,11 +18,14 @@ for nome_arquivo in os.listdir(pasta_xml):
             root = tree.getroot()
             ns = {'nfe': 'http://www.portalfiscal.inf.br/nfe'}
             
+            
             nNF = root.find('.//nfe:ide/nfe:nNF', ns).text
             dhEmi = root.find('.//nfe:ide/nfe:dhEmi', ns).text
             dhRecbto = root.find('.//nfe:protNFe/nfe:infProt/nfe:dhRecbto', ns).text
             serie = root.find('.//nfe:ide/nfe:serie', ns).text
-            vPag = root.find('.//nfe:detPag/nfe:vPag', ns).text
+
+            #Here the XML XPath is out of alignment, since in our national XML, used in invoices, there can be more than one piece of data in the field, so we have to group them.
+            vPag = sum(float(elem.text) for elem in root.findall('.//nfe:detPag/nfe:vPag', ns))
 
             dados.append({
                 'Arquivo': nome_arquivo,
@@ -38,6 +41,10 @@ for nome_arquivo in os.listdir(pasta_xml):
             print(f"{nome_arquivo}")
         except AttributeError:
             print(f"{nome_arquivo}")
+            
+        #Here, we need to add another exception of error.
+        except Exception as e:
+            print(f"{nome_arquivo}: {e}")
 
 df = pd.DataFrame(dados)
 df.to_excel('notas_fiscais.xlsx', index=False)
